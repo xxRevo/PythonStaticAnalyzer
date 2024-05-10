df770673511870224a0e64bb54dd7919bf96d93a
Berkem <berkem_elgul@hotmail.com>
2024-05-10 18:10:17 +0300

    multithreading,html

--- index.py
+++ index.py
@@ -12,8 +12,18 @@ import rarfile # Compressed file detection
 import py7zr # Compressed file detection
 import subprocess # For Strings command
 import pefile # For PE header analysis
+import peutils # For PE packer analysis
 import os # For PE header analysis
 import datetime # For PE header analysis
+import webbrowser # For displaying results as HTML
+import threading # For multithreading
+
+# --------------------------------------HTML--------------------------------------
+
+def write_html(html_content, filename):
+    with open(filename, 'w') as file:
+        file.write(html_content)
+    webbrowser.open('file://' + os.path.realpath(filename))
 
 # ---------------------------------------.PDF--------------------------------------- 
 
@@ -64,7 +74,7 @@ def check_zip_password(file_path): # ZIP password
     try:
         with zipfile.ZipFile(file_path) as zf:
             if zf.testzip() is None:
-                return "ZIP file is not password protected or is valid."
+                return "ZIP file is not password protected."
             else:
                 return "ZIP file might be corrupted."
     except RuntimeError as e:
@@ -80,7 +90,7 @@ def check_rar_password(file_path): # RAR password
     except rarfile.NeedPassword:
         return "RAR file is password protected."
 
-def check_7z_password(file_path):  # 7Z password
+def check_7z_password(file_path):  # 7ZIP password
     try:
         with py7zr.SevenZipFile(file_path, mode='r') as z:
             z.extractall()
@@ -148,7 +158,7 @@ def get_filetype(filename): # Main function to handle filetype operations
 
 # ----------------------------------------PE----------------------------------------
 
-def extract_strings(filepath):
+def extract_strings(filepath,html_content):
     try:
         # Run the strings command
         result = subprocess.run(['strings', filepath], capture_output=True, text=True)
@@ -165,26 +175,27 @@ def extract_strings(filepath):
                 urls.extend(url_pattern.findall(i))
                 ips.extend(ip_pattern.findall(i))
                 domains.extend(domain_pattern.findall(i))
-            print("URLs:")
+            html_content = html_content + """<h1>Strings</h1><table>
+            <tr><th>Category</th><th>String</th></tr>"""
             for i in urls:
-                print(i)
-            print("----------\nIPs:")
+                html_content = html_content + f"""<tr><td>URL</td><td>{i}</td></tr>"""
             for i in ips:
-                print(i)
-            print("----------\nDomains:")
+                html_content = html_content + f"""<tr><td>IP</td><td>{i}</td></tr>"""
             for i in domains:
-                print(i)
-            print("\n")
+                html_content = html_content + f"""<tr><td>Domain</td><td>{i}</td></tr>"""
+            html_content = html_content + """</table>"""
+            return html_content
         else:
-            return result.stderr
+            html_content = html_content + "<h1>Strings</h1><h2>Strings command yielded no results.</h2>"
+            return html_content
     except FileNotFoundError:
-        return "strings command not found."
+        return html_content
     
-def analyze_header(filepath):
+def analyze_header(filepath,html_content):
     pe = pefile.PE(filepath)
     data = {
-        'Architecture': 'x64' if pe.FILE_HEADER.Machine == 0x8664 else 'x86',
-        'Entropy': 0,
+        'Architecture': 'x86-x64' if pe.FILE_HEADER.Machine == 0x8664 else 'x86',
+        'Entropy': 0, #fix
         'File Size': os.path.getsize(filepath),
         'Number of Sections': len(pe.sections),
         'Compilation Date': datetime.datetime.utcfromtimestamp(pe.FILE_HEADER.TimeDateStamp).strftime('%Y-%m-%d %H:%M:%S'),
@@ -207,51 +218,125 @@ def analyze_header(filepath):
         for entry in pe.DIRECTORY_ENTRY_IMPORT:
             dll_name = entry.dll.decode()
             imported_dlls.append(dll_name)
-    
-    print("\nGENERAL INFORMATION")
-    print("--------------------")
-    for key,value in data.items():
-        print(key,":",value)
-    
-    print("\nSECTION INFORMATION")
-    print("--------------------")
-    for i in sections:
-        for key,value in i.items():
-            print(key,":",value)
+    html_addition = f"""    
+        <h1>General Information</h1>
+            <h2>Architecture: {data['Architecture']}</h2>
+            <h2>Entropy: {data['Entropy']}</h2>
+            <h2>File Size (In Bytes): {data['File Size']}</h2>
+            <h2>Sections: {data['Number of Sections']}</h2>
+            <h2>Compilation Date: {data['Compilation Date']}</h2>
+        <h1>Imported DLLs</h1>
+        <table>
+            <tr><th>DLL</th></tr>
+    """
+    html_content = html_content + html_addition
+    for dll in imported_dlls:
+        html_content = html_content + f"<tr><td>{dll}</td>"
+    html_content = html_content + "</table><h1>Section Information</h1><table><tr><th>Name</th><th>Entropy</th><th>Virtual Size</th><th>Raw Size</th></tr>"
+    for section in sections:
+        html_content = html_content + f"<tr><td>{section['Name']}</td><td>{section['Entropy']}</td><td>{section['Virtual Size']}</td><td>{section['Raw Size']}</td></tr>"
+    html_content = html_content + "</table>"
+    return html_content,sections
 
-    print("\nIMPORT INFORMATION")
-    print("--------------------")
-    for i in imported_dlls:
-        print(i)
-    
-    analyze_packing(sections)
-
-def analyze_packing(sections): # Check for packing status
+def analyze_packing(sections,html_content): # Check for packing status
+    packing_probability = 0
     for i in sections:
-        if(i['Entropy'] > 7.0):
-            print("Section",i['Name'],"has high entropy:",i['Entropy'])
-        if(i['Virtual Size'] / i['Raw Size'] > 2.0):
-            print("Section",i['Name'],"has high virtual size compared to raw size, Virtual Size: ",i['Virtual Size'],"Raw Size:",i['Raw Size'])
+        if(i['Entropy'] > 6.0):
+            html_content = html_content + f"""<h2>- The section {i['Name']} has high entropy value of {i['Entropy']}</h2>"""
+            if(i['Entropy'] > 7.5):
+                packing_probability = 100
+            elif(i['Entropy'] > 7.0):
+                packing_probability = 90
+            elif(i['Entropy'] > 6.5):
+                packing_probability = 75
+            else:
+                packing_probability = 50
+        if(i['Raw Size'] == 0): # Avoiding division by Zero
+            i['Raw Size'] = 1
+        if(i['Virtual Size'] / i['Raw Size'] > 10.0):
+            html_content = html_content + f"""<h2>- The section {i['Name']} has high Virtual to Raw size ratio with value {i['Virtual Size'] / i['Raw Size']}.</h2>"""
+    if packing_probability == 100:
+        html_content = html_content + "<h2>This file is certainly packed using an algortihm that is not in the signature database.</h2>"
+    elif packing_probability == 90:
+        html_content = html_content + "<h2>This file is almost certainly packed using an algortihm that is not in the signature database.</h2>"
+    elif packing_probability == 75:
+        html_content = html_content + "<h2>This file is most likely packed using an algortihm that is not in the signature database.</h2>"
+    elif packing_probability == 50:
+        html_content = html_content + "<h2>This file may be packed using an algortihm that is not in the signature database.</h2>"
+    else:
+        html_content = html_content + "<h2>This file does not appear to be packed.</h2>"
+    return html_content
+
+def load_signature_database(filepath): # Helper function for find_packer()
+    with open(filepath, 'r', encoding='latin-1') as file:
+        signature_data = file.read()
+    return peutils.SignatureDatabase(data=signature_data)
+
+def find_packer(filepath,sections,html_content):
+    try:
+        pe = pefile.PE(filepath)
+        signatures = load_signature_database("UserDB.TXT") # signature database from http://woodmann.com/BobSoft/Files/Other/UserDB.zip
+        result = signatures.match(pe, ep_only=True)
+        if result:
+            html_content = html_content + f"""<h1>Packer Information</h1><h2>This file was packed using the following packer: {result}</h2>"""
+            return html_content
+        else:
+            html_content = html_content + f"""<h1>Packer Information</h1><h2>No packing algorithm matches the file, manual analysis found the following result:<h2>"""
+            html_content = analyze_packing(sections,html_content)
+            return html_content
+    except Exception as err:
+        print(err)
+        return html_content
 
 # ---------------------------------------MAIN---------------------------------------
 
-filename = input("Enter Filename: ")
-filetype = get_filetype(filename)
-compressed_list = ["application/zip","application/x-rar","application/x-7z-compressed"]
-if (filetype == "pdf"):
-    if( not is_password_protected_pdf(filename)):
-        get_pdf_contents(filename)
-elif (filetype == "vnd.openxmlformats-officedocument.wordprocessingml.document"):
-    if(not is_encrypted_docx(filename)):
-        if(not is_password_protected_docx(filename)):
-            detect_language(filename)
-            has_macros(filename)
-elif(filetype == "application/zip"):
-    check_zip_password(filename)
-elif(filetype == "application/x-rar"):
-    check_rar_password(filename)
-elif(filetype == "application/x-7z-compressed"):
-    check_7z_password(filename)
-elif(filetype == "executable" or filetype == "x-dosexec"):
-    extract_strings(filename)
-    analyze_header(filename)
+def main_func(filename):
+    html_content = f"""
+        <html>
+        <head>
+            <title>{filename}</title>
+            """+"""<style>
+                body { font-family: Arial, sans-serif; }
+                table { width: 100%; border-collapse: collapse; }
+                th, td { border: 1px solid #ddd; padding: 8px; text-align: left; }
+                th { background-color: #f2f2f2; }
+            </style>
+        </head>
+        <body>"""
+    filetype = get_filetype(filename)
+    if (filetype == "pdf"):
+        if( not is_password_protected_pdf(filename)):
+            get_pdf_contents(filename)
+    elif (filetype == "vnd.openxmlformats-officedocument.wordprocessingml.document"):
+        if(not is_encrypted_docx(filename)):
+            if(not is_password_protected_docx(filename)):
+                detect_language(filename)
+                has_macros(filename)
+    elif(filetype == "application/zip"):
+        check_zip_password(filename)
+    elif(filetype == "application/x-rar"):
+        check_rar_password(filename)
+    elif(filetype == "application/x-7z-compressed"):
+        check_7z_password(filename)
+    elif(filetype == "executable" or filetype == "x-dosexec"):
+        html_content, sections = analyze_header(filename,html_content)
+        html_content = find_packer(filename,sections,html_content)
+        html_content = extract_strings(filename,html_content)
+    html_content = html_content + """</body></html>"""
+    write_html(html_content,'./outputs/'+filename+'.html')
+
+
+print("Enter filenames that you want to analyze, for entry of multiple files separate them by ',' character.")
+filenames = input("Enter Filename(s): ")
+threads = []
+if (filenames.count(",") > 0):
+    for i in filenames.split(","):
+        i = i.strip()
+        thread = threading.Thread(target=main_func, args=(i,))
+        threads.append(thread)
+        thread.start()
+else:
+    main_func(filenames.strip())
+
+for thread in threads:
+    thread.join()
